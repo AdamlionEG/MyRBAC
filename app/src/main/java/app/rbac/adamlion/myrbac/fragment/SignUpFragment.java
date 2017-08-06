0f77921c801fd5b2849b3a4e3ac4d296d753c4ce
AdamLionEG <029412642a>
2017-08-06 12:12:54 +0700

    before lunch 6/8/2560 12:00

---
+++ app/src/main/java/app/rbac/adamlion/myrbac/fragment/SignUpFragment.java
@@ -0,0 +1,25 @@
+package app.rbac.adamlion.myrbac.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.rbac.adamlion.myrbac.R;
+
+/**
+ * Created by User on 6/8/2560.
+ */
+
+public class SignUpFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        return view;
+
+    }
+}   //Main Class
